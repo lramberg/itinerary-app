f69db42b121f370f013df280004ed02f024e9dbf
Lyndsay Ramberg <lyndsayramberg@Lyndsays-MacBook-Pro.local>
2019-05-30 21:04:58 -0700

    fix user functionality

--- controllers/itineraries.js
+++ controllers/itineraries.js
@@ -1,5 +1,6 @@
 var Itinerary = require('../models/itinerary');
 var Port = require('../models/port');
+var Member = require('../models/member');
 
 module.exports = {
     index,
@@ -14,80 +15,69 @@ module.exports = {
 
 function edit(req, res, next) {
     Itinerary.findById(req.params.id)
-        .populate('ports').exec(function(err, itinerary){
-            Port.find({_id: {$in: itinerary.ports}})
-            .exec(function(err, ports){
-                res.render('itineraries/edit', {
-                    itinerary,
-                    ports
-                });
-            })
-        }) 
+        .populate('ports').exec(function (err, itinerary) {
+            Port.find({ _id: { $in: itinerary.ports } })
+                .exec(function (err, ports) {
+                    res.render('itineraries/edit', {
+                        itinerary,
+                        ports
+                    });
+                })
+        })
 }
-
-// function removePort(req, res) {
-//     Itinerary.findById(req.params.id)
-//     .populate('ports').exec(function(err, itinerary) {
-//         itinerary.ports.update( { _id }, {$pullAll: {ports: _id}} );
-//     });
-    // Itinerary.findById(req.params.id)
-    // .populate('ports').exec(function(err, itinerary) {
-    //     Port.find({_id: {$in: itinerary.ports}})
-    //     .exec(function(err, ports){
-    //         itinerary.ports
-    //     })
-    // })
-
-    // hit DELETE port button to remove port from itinerary
-    // grab the port's id, check to see where it is in the `itinerary.ports` array (try indexOf)
-    // once you've found the index of that partiuclar port, remove it from the itinerary.ports array
-// }
 
 function update(req, res) {
     console.log('req.body: ', req.body);
-    Itinerary.findByIdAndUpdate(req.params.id, req.body, function(err, itinerary) {        
-        itinerary.save(function(err) {
+    Itinerary.findByIdAndUpdate(req.params.id, req.body, function (err, itinerary) {
+        itinerary.save(function (err) {
             res.redirect(`/itineraries/${req.params.id}`);
         });
     });
 }
 
 function deleteItin(req, res, next) {
-    Itinerary.findByIdAndDelete(req.params.id, function(err) {
+    Itinerary.findByIdAndDelete(req.params.id, function (err) {
         res.redirect('/itineraries');
     })
 }
 
 function addPort(req, res) {
-    Itinerary.findById(req.params.id, function(err, itinerary) {
+    Itinerary.findById(req.params.id, function (err, itinerary) {
         itinerary.ports.push(req.body.portId);
         console.log('itinerary ', Itinerary);
-        itinerary.save(function(err) {
+        itinerary.save(function (err) {
             res.redirect(`/itineraries/${itinerary._id}`);
         });
     });
 }
 
 function show(req, res) {
-    Itinerary.findById(req.params.id)
-    .populate('ports').exec(function(err, itinerary) {
-        Port.find({})
-        .exec(function(err, ports) {
-            res.render('itineraries/show', {
-                itinerary,
-                ports
+    Member.find({}).exec(function (err, members) {
+        Itinerary.findById(req.params.id)
+            .populate('ports').exec(function (err, itinerary) {
+                Port.find({}).exec(function (err, ports) {
+                    if (req.user) {
+                        Member.findById(req.user._id).exec(function (err, sessionUser) {
+                            res.render('itineraries/show', { itinerary, ports, sessionUser });
+                        });
+                    } else {
+                        res.render('itineraries/show', { itinerary, ports, sessionUser: null });
+                    }
+                });
             });
-            console.log('ports: ', ports);
-        });
-    }); 
+    });
 }
 
 function create(req, res) {
-    Itinerary.create(req.body, function(err, itinerary) {
-        req.user.itineraries.push(itinerary);
-        req.user.save(function(err){
-            res.redirect('/itineraries');
-        })
+    Member.findById(req.user._id).exec(function(err, member) {
+        Itinerary.create(req.body, function (err, itinerary) {
+            itinerary.userId = member._id;
+            req.user.itineraries.push(itinerary);
+            itinerary.save();
+            req.user.save(function (err) {
+                res.redirect('/itineraries');
+            })
+        });
     });
 }
 
@@ -96,9 +86,11 @@ function newItinerary(req, res) {
 }
 
 function index(req, res) {
-    Itinerary.find({}, function(err, itineraries) {
-        res.render('itineraries/index', {
-            itineraries
+    Member.findById(req.user._id).exec(function(err, sessionUser){
+        Itinerary.find({}, function (err, itineraries) {
+            res.render('itineraries/index', {
+                itineraries, sessionUser
+            });
         });
     });
 }
--- models/itinerary.js
+++ models/itinerary.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 var itinerarySchema = new Schema ({
     name: String,
     description: String,
+    userId: String,
     ports: [{type: Schema.Types.ObjectId, ref: 'Port'}]
 });
 
